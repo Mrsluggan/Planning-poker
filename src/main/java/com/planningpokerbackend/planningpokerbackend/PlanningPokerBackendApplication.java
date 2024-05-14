3266c68f93a858729aa0366dc01be17df3ebc8ee
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-05-14 10:13:51 +0200

    dependencies

---
+++ src/main/java/com/planningpokerbackend/planningpokerbackend/PlanningPokerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.planningpokerbackend.planningpokerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningPokerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningPokerBackendApplication.class, args);
+	}
+
+}
