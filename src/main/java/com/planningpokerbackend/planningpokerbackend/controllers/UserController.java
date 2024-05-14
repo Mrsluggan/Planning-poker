f99db9145d6b05103b74b8e6f0fb659bc2939158
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-05-14 13:33:29 +0200

    grunden för service/controllers

--- src/main/java/com/planningpokerbackend/planningpokerbackend/controllers/UserController.java
+++ src/main/java/com/planningpokerbackend/planningpokerbackend/controllers/UserController.java
@@ -1,5 +1,23 @@
 package com.planningpokerbackend.planningpokerbackend.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import com.planningpokerbackend.planningpokerbackend.services.ProjectService;
+import com.planningpokerbackend.planningpokerbackend.services.TaskService;
+import com.planningpokerbackend.planningpokerbackend.services.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class UserController {
+
+    private UserService userservice;
+    private TaskService taskservice;
+    private ProjectService projectService;
     
+    public UserController( UserService userservice, TaskService taskservice, ProjectService projectService) {
+        this.projectService = projectService;
+        this.taskservice = taskservice;
+        this.userservice = userservice;
+    }
+
 }
--- src/main/java/com/planningpokerbackend/planningpokerbackend/services/UserService.java
+++ src/main/java/com/planningpokerbackend/planningpokerbackend/services/UserService.java
@@ -1,5 +1,16 @@
 package com.planningpokerbackend.planningpokerbackend.services;
 
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
     
+    private final MongoOperations mongoOperations;
+
+    public UserService (MongoOperations mongoOperations) {
+        this.mongoOperations=mongoOperations;
+    }
 }
+
+
