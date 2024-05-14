fa104f1fe12bbe1a44c282b8dbc42f0ec1d0fa72
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-05-14 13:46:35 +0200

    fixed #13

--- src/main/java/com/planningpokerbackend/planningpokerbackend/services/UserService.java
+++ src/main/java/com/planningpokerbackend/planningpokerbackend/services/UserService.java
@@ -1,7 +1,11 @@
 package com.planningpokerbackend.planningpokerbackend.services;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
+
+import com.planningpokerbackend.planningpokerbackend.models.User;
 
 @Service
 public class UserService {
@@ -10,6 +14,10 @@ public class UserService {
 
     public UserService (MongoOperations mongoOperations) {
         this.mongoOperations=mongoOperations;
+    }
+
+    public List <User> getAllUsers() {
+        return mongoOperations.findAll(User.class);
     }
 }
 
