0abecdef4b8896582365857fb4c8f9c8e380fc43
mrsluggan <ericflyger@gmail.com>
2024-05-15 11:40:29 +0200

    Taskmapping + service

--- src/main/java/com/planningpokerbackend/planningpokerbackend/controllers/TaskController.java
+++ src/main/java/com/planningpokerbackend/planningpokerbackend/controllers/TaskController.java
@@ -1,23 +1,65 @@
 package com.planningpokerbackend.planningpokerbackend.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.planningpokerbackend.planningpokerbackend.models.Task;
 import com.planningpokerbackend.planningpokerbackend.services.ProjectService;
 import com.planningpokerbackend.planningpokerbackend.services.TaskService;
 import com.planningpokerbackend.planningpokerbackend.services.UserService;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class TaskController {
-    
+
     private TaskService taskservice;
     private ProjectService projectService;
     private UserService userservice;
-    
+
     public TaskController(TaskService taskservice, ProjectService projectService, UserService userservice) {
         this.taskservice = taskservice;
         this.projectService = projectService;
         this.userservice = userservice;
     }
-    
+
+    @GetMapping("/getTasks")
+    public List<Task> getAllTasks() {
+        return taskservice.getAllTasks();
+    }
+
+    @GetMapping("/getTasks/{projectId}")
+    public List<Task> getAllTaskFromProjectId(@PathVariable("projectId") String id) {
+        return taskservice.getTasksByProjectId(id);
+    }
+
+    @GetMapping("/getTask/{taskId}")
+    public Task getTaskById(@PathVariable("taskId") String id) {
+        return taskservice.getTaskById(id);
+    }
+
+    @PostMapping("/newTask/{projectId}")
+    public Task createNewTask(@PathVariable("projectId") String id, Task task) {
+        return taskservice.createNewTask(id, task);
+    }
+
+    @PutMapping("/handleTask/{taskId}")
+    public Task startTask(@PathVariable("taskId") String id) {
+
+        Task task = taskservice.getTaskById(id);
+        if (!task.isTimerRunning()) {
+            return taskservice.startTask(id);
+        } else {
+            return taskservice.stopTask(id);
+
+        }
+
+    }
+
 }
--- src/main/java/com/planningpokerbackend/planningpokerbackend/services/TaskService.java
+++ src/main/java/com/planningpokerbackend/planningpokerbackend/services/TaskService.java
@@ -1,16 +1,55 @@
 package com.planningpokerbackend.planningpokerbackend.services;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import com.planningpokerbackend.planningpokerbackend.models.Task;
+import com.planningpokerbackend.planningpokerbackend.models.User;
 
 @Service
 public class TaskService {
-    
+
     private final MongoOperations mongoOperations;
 
-    public TaskService (MongoOperations mongoOperations) {
-        this.mongoOperations=mongoOperations;
+    public TaskService(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    public List<Task> getAllTasks() {
+        return mongoOperations.findAll(Task.class);
+    }
+
+    public Task getTaskById(String taskId) {
+        return mongoOperations.findById(taskId, Task.class);
+    }
+
+    public List<Task> getTasksByProjectId(String projectId) {
+        Query query = Query.query(Criteria.where("projectId").is(projectId));
+        return mongoOperations.find(query, Task.class);
+    }
+
+    public Task createNewTask(String projectId, Task task) {
+        task.setProjectId(projectId);
+        return mongoOperations.save(task);
+    }
+
+    public void removeTask(String taskId) {
+        mongoOperations.remove(getTaskById(taskId));
+    }
+
+    public Task startTask(String taskId) {
+        Task task = getTaskById(taskId);
+        task.startTimer();
+        return mongoOperations.save(task);
+    }
+
+    public Task stopTask(String taskId) {
+        Task task = getTaskById(taskId);
+        task.pauseTimer();
+        return mongoOperations.save(task);
     }
 }
-
-
