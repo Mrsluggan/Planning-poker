7f5419340e1a4620a8847b90a7af36b96f8a501a
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-05-15 10:27:22 +0200

    fixed #9

--- src/main/java/com/planningpokerbackend/planningpokerbackend/controllers/UserController.java
+++ src/main/java/com/planningpokerbackend/planningpokerbackend/controllers/UserController.java
@@ -12,6 +12,8 @@ import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -37,5 +39,8 @@ public class UserController {
         return userService.getUserById(id);
     }
 
-    
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        return userService.registerUser(user.getUsername(), user.getPassword());
+    }
 }
--- src/main/java/com/planningpokerbackend/planningpokerbackend/services/UserService.java
+++ src/main/java/com/planningpokerbackend/planningpokerbackend/services/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.planningpokerbackend.planningpokerbackend.models.User;
 
@@ -24,6 +25,25 @@ public class UserService {
     public List <User> getUserById(String id) {
         Query query = Query.query(Criteria.where("id").is(id));
         return mongoOperations.find(query, User.class);
+    }
+
+    public User getUsername(String username) {
+        Query query = Query.query(Criteria.where("username").is(username));
+        return mongoOperations.findOne(query,User.class);
+    }
+
+    public void createUser(User user) {
+        mongoOperations.save(user);
+    }
+
+    public String registerUser(String username, String password) {
+        if(getUsername(username) == null) {
+            User user = new User(username, password);
+            createUser(user);
+            return "success";
+        } else {
+            return "Användarnamnet är upptaget";
+        }
     }
 }
 
